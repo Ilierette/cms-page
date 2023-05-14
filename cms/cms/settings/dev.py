0d5d2013ba58922f4d2bf2f12334b6cbcfdc328e
Karolina Pietraś <Ilierette@users.noreply.github.com>
2023-05-15 00:08:28 +0200

    python init

---
+++ cms/cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-3c(%nx5)hnl26_t#08wk-ck)i0om6(v+10%=9!86!6+v8t#@tx"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
